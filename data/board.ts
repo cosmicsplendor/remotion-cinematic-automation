7c0468cd343726b10991f4006dd85fb68d4b7eb6
Umesh KC <ufocalypse@gmail.com>
2025-05-08 14:10:45 +0545

    Add parallax hook data for frog boys

--- data/board.ts
+++ data/board.ts
@@ -1,40 +1,40 @@
 export default [
     {
         id: "1",
-        name: "चिओल-वोन",
-        subtitle: "१३ वर्षको नेता, ताइक्वान्डो लडाकू",
+        name: "चिओल-वोन", // Woo Cheol-won in Nepali
+        subtitle: "१३ वर्षको, समूहको नाइके, तेक्वान्दो खेलाडी", // Age 13, Leader of the pact, Taikwando player in Nepali
         photoUrl: "/assets/board/images/1.png",
         audioUrl: "/assets/board/audio/1.wav",
-        audioDuration: 17
+        audioDuration: 16.4
     },
     {
         id: "2",
-        name: "योन-ग्यु",
-        subtitle: "१२ वर्षको चिन्तक र जिज्ञासु",
+        name: "किम योंग-ग्यु", // Kim Yeong-gyu in Nepali
+        subtitle: "१२ वर्षको, चिन्तक र जिज्ञासु", // Age 11, Curious Thinker in Nepali
         photoUrl: "/assets/board/images/2.png",
         audioUrl: "/assets/board/audio/2.wav",
-        audioDuration: 13
+        audioDuration: 8.4
     },
     {
         id: "3",
-        name: "पार्क चान-इन",
-        subtitle: "११ वर्षको, दयालु र मृदु",
+        name: "पार्क चान-इन", // Park Chan-in in Nepali
+        subtitle: "११ वर्षको, हँसिलो", // Age 11, Cheerful in Nepali
         photoUrl: "/assets/board/images/3.png",
         audioUrl: "/assets/board/audio/3.wav",
         audioDuration: 5.5
     },
     {
         id: "4",
-        name: "जो हो-योन",
-        subtitle: "१० वर्षको, उत्साही र हर्षित",
+        name: "जो हो-योन", // Jo Ho-yeon in Nepali
+        subtitle: "१० वर्षको, दयालु र नम्र स्वभावको", // Age 10, kind and gentle mannered in Nepali
         photoUrl: "/assets/board/images/4.png",
         audioUrl: "/assets/board/audio/4.wav",
         audioDuration: 10.8
     },
     {
         id: "5",
-        name: "किम जोंग-सिक",
-        subtitle: "९ वर्षको, ठूला केटाहरुप्रति श्रद्धा गर्ने",
+        name: "किम जोंग-सिक", // Kim Jong-sik in Nepali
+        subtitle: "९ वर्षको, ठूला केटाहरूलाई सुपरहिरो ठान्ने", // Age 9, saw bigger boys as superheroes in Nepali (corrected typo "biger boyas")
         photoUrl: "/assets/board/images/5.png",
         audioUrl: "/assets/board/audio/5.wav",
         audioDuration: 10.8
