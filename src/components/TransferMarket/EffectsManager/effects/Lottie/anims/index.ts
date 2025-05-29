fa9d60ccaae4e7e607626e43bb13542feaccb8c7
Umesh KC <ufocalypse@gmail.com>
2025-05-29 21:42:22 +0545

    Save the progress so far

--- src/components/TransferMarket/EffectsManager/effects/Lottie/anims/index.ts
+++ src/components/TransferMarket/EffectsManager/effects/Lottie/anims/index.ts
@@ -3,13 +3,15 @@ import wow from "./wow.json";
 import rocket1 from "./rocket1.json";
 import skull from "./skull.json";
 import fire from "./fire.json";
+import tick1 from "./tick1.json";
 
 const anims = {
     rocketman,
     wow,
     rocket1,
     skull,
-    fire
+    fire,
+    tick1
 }
 
 export default anims;
